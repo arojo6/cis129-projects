79c64f6e2c0d43dee4b93865e29731d758359d80
arojo6 <arojo6@mail.pima.edu>
2024-09-19 22:24:04 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Author: Alondra Rojo
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+
+print('There was a wise turtle named fred who had been a travelling saxaphone player.')
+print('One rainy day Fred encountered an abandoned puppy named petunia')
+print('He adopted her and taught her the ways of jazz')
+print('Together they created a jazz band slowly recruiting more and more to their musical family')
